cbd51b14498e62c6204abdf9eab3878f47afbb02
David Comerford <dave@swizzling.net>
2016-11-27 23:10:50 +0000

    Added some user interaction. I may have overcooked this a little.

--- install.sh
+++ install.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-CACTIPATH = "/var/www/html/cacti"
+CACTIPATH="/var/www/html/cacti"
 
-cp -v cacti-vpshere.py $CACTIPATH/scripts/
-cp -v vcenters.conf $CACTIPATH/scripts/
+function doTheThings {
+   cp -v cacti-vpshere.py $CACTIPATH/scripts/
+   cp -v vcenters.conf $CACTIPATH/scripts/
+}
+
+echo $CACTIPATH
+
+echo "Does this Cacti path look Ok?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) doTheThings; break;;
+        No ) echo "Fix the path variable in $0 " ; exit;;
+    esac
+done
